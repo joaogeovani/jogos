751adb2c52e216df02f675a1fde46ba664588334
joaogeovani <joao-geovani@hotmail.com>
2023-06-19 21:54:43 -0300

    Atualizei o jogo adicionando um número random, uma escolha de quantia de rodadas e uma maneira de mostrar pontuação de acordo com as tentativas do usuário.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,25 @@
+import random
+
 print("*****************")
 print("Bem vindo ao jogo")
 print("Adivinhe o número")
 print("*****************")
 
-numeroComputador = 47
-tentativasMaximas = 3
+numeroComputador = random.randrange(1, 101)
+tentativasMaximas = 0
+pontos = 1000
+
+print("Qual desafio você deseja?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = input("Escolha uma dificuldade: ")
+
+if (nivel == 1):
+    tentativasMaximas = 20
+elif (nivel == 2):
+    tentativasMaximas = 10
+else:
+    tentativasMaximas = 5
 
 for rodada in range(1, tentativasMaximas +1):
     print("Tentativa {} de {}".format(rodada, tentativasMaximas))
@@ -21,12 +36,21 @@ for rodada in range(1, tentativasMaximas +1):
     menor = chuteInt < numeroComputador
 
     if (acertou):
-        print("Você acertou o número!")
+        print("Você acertou o número e fez {} pontos!".format(pontos))
         break
     else:
         if (maior):
             print("Você errou! O número é maior.")
         elif (menor):
             print("Você errou! o número é menor.")
+
+        pontosPerdidos = abs(numeroComputador - chuteInt)
+        pontos = pontos - pontosPerdidos
+
+        if (rodada == tentativasMaximas):
+            print("*************************")
+            print("Suas tentativas acabaram!")
+            print("*************************")
+            print("O número secreto era {}. Você fez {} pontos".format(numeroComputador, pontos))
 
 print("Fim do jogo")
