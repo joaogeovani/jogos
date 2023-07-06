9e219d14ba73052fe83cdb2f3a8ce6edf7b05c21
joaogeovani <joao-geovani@hotmail.com>
2023-07-06 19:52:24 -0300

    Montando o começo do jogo da forca.

--- forca.py
+++ forca.py
@@ -4,6 +4,24 @@ def jogoDaForca():
     print("Bem vindo ao jogo da forca")
     print("**************************")
 
+    palavraSecreta = "gato"
+
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+
+        escolhaLetra = input("Qual letra? ")
+        escolhaLetra = escolhaLetra.strip()
+
+        index = 0
+        for letra in palavraSecreta:
+            if (escolhaLetra.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if (__name__ == "__main__"):
